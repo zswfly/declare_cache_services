a5ead4e664e20906dfd0a9013a0b900e3ee1214b
zhangshaowei <zhangshaowei@iplas.com.cn>
2020-04-02 22:56:52 +0800

    new

--- src/main/java/com/zsw/services/TestServices.java
+++ src/main/java/com/zsw/services/TestServices.java
@@ -1,6 +1,5 @@
 package com.zsw.services;
 
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
